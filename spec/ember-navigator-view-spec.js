0ece43d31f4d77886494250f8db488a8d4f70e4f
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2016-09-03 11:44:39 +0200

    Initial commit

---
+++ spec/ember-navigator-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import EmberNavigatorView from '../lib/ember-navigator-view';
+
+describe('EmberNavigatorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
